e50aede69678446f91b58048f3adaf13d2b9eb3a
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2024-07-09 17:08:15 +0600

    added csrf trussted origins

--- LMS/settings.py
+++ LMS/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://library-management-system-7gaw.onrender.com/','https://*.127.0.0.1']
 
 
 # Application definition
